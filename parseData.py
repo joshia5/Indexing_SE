a6734283dce70a11006892b350f003123188e9c4
mra-ruiz <ruizcm20@gmail.edu>
2020-12-04 11:00:53 -0500

    Function to parse json file

--- parseData.py
+++ parseData.py
@@ -1,1 +1,18 @@
 import json
+
+def parseData():
+    # Opening JSON file 
+    f = open('doc1_info.json', "r") 
+
+    # returns JSON object as a dictionary 
+    data = json.loads(f.read())
+
+    # Take data from json file and add to dictionary
+    websiteWordData = {}
+    for i in data: 
+        websiteWordData[i] = data[i]
+        
+    # Closing file 
+    f.close()
+
+parseData()
