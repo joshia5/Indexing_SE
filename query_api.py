f60c9863037725c67ad951fc64be6d1339480b8c
Jason-Durr <durrj2@rpi.edu>
2020-12-08 10:42:23 -0500

    Updated calls for querying and parse data"

--- query_api.py
+++ query_api.py
@@ -1,12 +1,8 @@
 import flask
 from parseData import parseData
+from index import *
 
-
-
-
-
-
-
+index = Index()
 
 
 app = flask.Flask(__name__)
@@ -15,14 +11,19 @@ app.config["DEBUG"] = True
 
 @app.route('/query', methods=['GET'])
 def query_handler():
-        query_string = flask.request.args.get("query")
-        return query_string 
+    query_string = flask.request.args.get("query")
+    result = index.find_entry(query_string)
+    return flask.jsonify(result)
+
+
 @app.route('/add_file', methods=['POST'])
 def add_file_handler():
     file_json = flask.request.get_json(force=True)
-    parseData(file_json)
-    return flask.jsonify(isError= False,
-                        message= "Success",
-                        statusCode= 200,
-                        data= ""), 200
-app.run(host='0.0.0.0',port=80)
+    parseData(file_json, index)
+    return flask.jsonify(isError=False,
+                         message="Success",
+                         statusCode=200,
+                         data=""), 200
+
+
+app.run(host='0.0.0.0', port=80)
